6e69dbbf15fc98aa9ffaffc86e76cd888bd5f417
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-06-27 16:31:11 +0200

    Jour 0.75

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,11 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testPublicUrlIsSuccessful(): void
+    public function provideUrlsAndStatusCodes(): array
+    {
+        return [
+            'index' => ['/', 200],
+            'contact' => ['/contact', 200],
+            'book index' => ['/book', 200],
+            'book show' => ['/book/1', 200],
+            'toto' => ['/toto', 404],
+        ];
+    }
+
+    /**
+     * @dataProvider provideUrlsAndStatusCodes
+     */
+    public function testPublicUrlIsSuccessful(string $uri, int $statusCode): void
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client->request('GET', $uri);
 
-        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame($statusCode);
     }
 }
