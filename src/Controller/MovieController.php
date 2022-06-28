56719f2f452e33a9af87e0c1298d2a839efcdd1a
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-06-28 09:58:23 +0200

    Jour 1,25

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/movie", name="app_movie_")
+ */
+class MovieController extends AbstractController
+{
+    /**
+     * @Route("", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('movie/index.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="details", methods={"GET"})
+     */
+    public function details(int $id): Response
+    {
+        $movie = [
+            'title' => 'Star Wars',
+            'releasedAt' => new \DateTime('1977-05-25'),
+            'genres' => ['Action', 'Adventure', 'Fantasy'],
+        ];
+
+        return $this->render('movie/details.html.twig', [
+            'movie' => $movie
+        ]);
+    }
+}
